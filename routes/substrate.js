d279aaa989172dac94922cd7820ba6ae22222495
antio789 <antio789@hotmail.fr>
2026-02-13 15:05:54 +0100

    modified download link to be a direct download instead of a onedrive link

--- routes/substrate.js
+++ routes/substrate.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ObjectsToCsv = require('objects-to-csv');
+const fs = require('fs');
 
 const {
     getPretreatmentMainFields,
@@ -9,6 +10,7 @@ const {
     getArticles,
     getSubstrate_categories, getarticlesinfo
 } = require('../models/substrate_articles');
+const {join} = require("node:path");
 
 
 router.get('/', async (req, res) => {
@@ -56,6 +58,37 @@ router.post('/export', async (req, res) => {
     const csv = await new ObjectsToCsv(Articles).toString();
     res.attachment('substrate.csv').send(csv);
 })
+
+router.get('/download', rateLimitDownload, (req, res) => {
+    const filePath = join(__dirname, '../Downloads/Biogas_visualisation.pbix');
+    const fileStream = fs.createReadStream(filePath);
+
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', 'attachment; filename="Biogas_visualisation.pbix"');
+
+    fileStream.pipe(res);
+
+    fileStream.on('error', (err) => {
+        console.error('File error:', err);
+        res.status(500).send('Error downloading file');
+    });
+});
+
+//basic setup to limit downloads, at some point update to a real solution
+function rateLimitDownload(req, res, next) {
+    const clientIP = req.ip; // req.headers['x-forwarded-for'] -proxy check mdn for usage
+    const cooldownMs = 1500;
+    if (downloadTracking.has(clientIP)) {
+        return res.status(429).send('Too many requests. Please wait before downloading again.');
+    }
+    const cleanupTimer = setTimeout(() => {
+        downloadTracking.delete(clientIP);
+    }, cooldownMs);
+    downloadTracking.set(clientIP, cleanupTimer);
+    next();
+}
+
+const downloadTracking = new Map();
 
 
 module.exports = router;
